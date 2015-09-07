74a9331b7b65ee7c899543cccc0ce3a63188365a
Volker Thiel <volker.thiel@mage-profis.de>
2015-09-07 11:53:21 +0200

    Fixed tab name and positioning of attribute, added field description

--- app/code/community/VinaiKopp/CategoryLink/sql/vinaikopp_categorylink_setup/install-0.1.0.php
+++ app/code/community/VinaiKopp/CategoryLink/sql/vinaikopp_categorylink_setup/install-0.1.0.php
@@ -6,12 +6,15 @@ $installer = Mage::getResourceModel('catalog/setup', 'catalog_setup');
 $installer->startSetup();
 
 $installer->addAttribute('catalog_category', VinaiKopp_CategoryLink_Helper_Data::ATTR_CODE_LINK, array(
-    'label' => 'Category Link URL',
-    'type' => 'varchar',
-    'input' => 'text',
+    'label'           => 'Category Link URL',
+    'type'            => 'varchar',
+    'input'           => 'text',
     'is_configurable' => 0,
-    'required' => 0,
-    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE
+    'required'        => 0,
+    'note'            => 'Must include protocol (http://, https://, ftp://, ...)',
+    'sort_order'      => 4,
+    'global'          => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    'group'           => 'General Information',
 ));
 
 $installer->endSetup();
